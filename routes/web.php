f50d576afc688f56a2e5c8071a967e1828f419e1
Alexander Justesen <alex.justesen@gmail.com>
2020-10-22 21:24:03 -0400

    Removed welcome view, sorry Taylor

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('welcome', function () {
-    return view('welcome');
-});
-
 Route::get('/', function () {
     return view('hello');
 });
