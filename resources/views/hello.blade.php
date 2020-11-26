ff73b7ef28f603c8ed715176a8d7f9c0c6c400f3
Alex Justesen <alexjustesen@users.noreply.github.com>
2020-11-25 20:20:19 -0500

    Updated description to match the title

--- resources/views/hello.blade.php
+++ resources/views/hello.blade.php
@@ -12,7 +12,7 @@
 
     {{-- Site title and description --}}
     <title>{{ config('app.name') }}</title>
-    <meta name="description" content="CI/CD pipeline with Lasso and GitHub actions.">
+    <meta name="description" content="Laravel CI/CD pipeline with Lasso and GitHub actions.">
 
     {{-- Stylesheets --}}
     <link href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,400&display=swap" rel="stylesheet">
