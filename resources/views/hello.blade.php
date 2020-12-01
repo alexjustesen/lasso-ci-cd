0cbf4434e65dba3cd3dcb5cd28b830ce9ab2aa40
Alexander Justesen <alex.justesen@gmail.com>
2020-12-01 08:35:16 -0500

    Added ko-fi support button

--- resources/views/hello.blade.php
+++ resources/views/hello.blade.php
@@ -44,6 +44,12 @@
                 <article class="prose prose-sm max-w-none lg:prose-xl">
                     <h1 id="home">Laravel CI/CD Pipeline with GitHub Actions and Lasso</h1>
 
+                    <div class="flex items-center space-x-4">
+                        <a href="https://ko-fi.com/S6S12TS60" target="_blank" rel="nofollow">
+                            <img src="https://cdn.ko-fi.com/cdn/kofi3.png?v=2" class="border-none h-8" style="margin: 0;" alt="Buy Me a Coffee at ko-fi.com">
+                        </a>
+                    </div>
+
                     <p>
                         This guide will walk you though how I use GitHub Actions to create a CI/CD pipeline for <a href="https://laravel.com" target="_blank" rel="noopener">Laravel</a> applications. We'll use
                         <a href="https://getlasso.dev" target="_blank" rel="noopener">Lasso</a> from <a href="https://github.com/Sammyjo20" target="_blank" rel="noopener">Sam Carré</a> to compile Webpack
