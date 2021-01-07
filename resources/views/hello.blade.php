feecbab5ab0cac09a922e4048e3e0c8be0f8df6d
Alex Justesen <alexjustesen@users.noreply.github.com>
2021-01-07 13:46:17 -0500

    Fixed header

--- resources/views/hello.blade.php
+++ resources/views/hello.blade.php
@@ -45,7 +45,7 @@
 
             <div class="p-4 pt-12 sm:p-6 lg:p-8">
                 <article class="prose-sm prose max-w-none lg:prose-xl">
-                    <h1 id="home">Laravel CI/CD Pipeline with GitHub Actions and Lasso</h1>
+                    <h1 id="home">Laravel CI/CD Pipeline with Lasso and GitHub Actions</h1>
 
                     <div class="flex items-center space-x-4">
                         <a href="https://ko-fi.com/S6S12TS60" target="_blank" rel="noopener">
