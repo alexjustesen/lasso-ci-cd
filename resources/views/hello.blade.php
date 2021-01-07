45b8ee90e5214ac928599989c4736d76e684aace
Alex Justesen <alexjustesen@users.noreply.github.com>
2021-01-07 16:41:28 -0500

    Updated mix and tailwindcss build processes

--- resources/views/hello.blade.php
+++ resources/views/hello.blade.php
@@ -370,8 +370,6 @@
     </div>
 
     <!-- Scripts -->
-    <script src="{{ mix('js/manifest.js') }}"></script>
-    <script src="{{ mix('js/vendor.js') }}"></script>
     <script src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,39 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-    future: {
-        removeDeprecatedGapUtilities: true,
-        purgeLayersByDefault: true,
-        defaultLineHeights: true,
-        standardFontWeights: true
-    },
     purge: [
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php'
     ],
+
     theme: {
         extend: {
             fontFamily: {
                 sans: ['Work Sans', ...defaultTheme.fontFamily.sans],
-            }
-        },
-        typography: {
-            default: {
-                css: {
-                    fontFamily: 'Work Sans',
-                    h2: {
-                        fontWeight: '600',
-                    },
-                    h3: {
-                        fontWeight: '500'
+            },
+
+            typography: {
+                DEFAULT: {
+                    css: {
+                        fontFamily: 'Work Sans',
+
+                        h2: {
+                            fontWeight: '600',
+                        },
+
+                        h3: {
+                            fontWeight: '500'
+                        }
                     }
                 }
             }
         }
     },
-    variants: {},
+
+    variants: {
+        //
+    },
+
     plugins: [
         require('@tailwindcss/typography')
     ]
