a4e72896fdd29b2db9e0eb8a0f611eb6a98da0af
Alex Justesen <alexjustesen@users.noreply.github.com>
2021-01-07 18:00:15 -0500

    Only show version and date in production

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,7 @@
 <div class="text-xs text-center text-white lg:text-sm">
-    Last updated on <span class="font-medium text-emerald-400">{{ \Carbon\Carbon::parse(\App\Application::DATE)->format('F jS, Y') }}</span> as <span class="font-medium text-emerald-400">v{{ \App\Application::VERSION }}</span>.
+    @if (config('app.env') == 'production')
+        Last updated on <span class="font-medium text-emerald-400">{{ \Carbon\Carbon::parse(\App\Application::DATE)->format('F jS, Y') }}</span> as <span class="font-medium text-emerald-400">v{{ \App\Application::VERSION }}</span>.
+    @else
+        Last updated on 🤷‍♂️, this is the <span class="font-medium text-emerald-400">{{ config('app.env') }}</span> environment.
+    @endif
 </div>
